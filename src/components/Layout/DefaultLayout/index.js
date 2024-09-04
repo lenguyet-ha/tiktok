1d03ad3ec8c08a96e607a46ba317a3a653989cec
Le Nguyet Ha <lenguyetha14@gmail.com>
2024-09-04 09:49:42 +0700

    UI cho header, phần 1

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,11 +1,15 @@
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx("container")}>
                 <Sidebar />
                 <div className="content">{children}</div>
             </div>
